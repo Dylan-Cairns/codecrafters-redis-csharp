c7ca576815aa491c1c0dc7eaaccca0d09d2c50f1
Dylan Cairns <dcairns@indicalab.com>
2024-11-06 14:44:05 -0700

    multiple pings

--- src/Server.cs
+++ src/Server.cs
@@ -5,8 +5,18 @@ using System.Text;
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
 
-// Uncomment this block to pass the first stage
+
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
 var clientSocket = server.AcceptSocket();
-clientSocket.Send(Encoding.UTF8.GetBytes("+PONG\r\n"));
+
+while (clientSocket.Connected)
+{
+
+    var buffer = new byte[1024];
+
+    await clientSocket.ReceiveAsync(buffer);
+
+    await clientSocket.SendAsync(Encoding.ASCII.GetBytes("+PONG\r\n"));
+
+}
